46b5e6ba01f69304e1a00b22ce87f19710b2a299
DOEKYONG <apochmn@naver.com>
2024-05-16 10:11:09 +0900

    1/useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,31 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [count, setCount] = useState(0);
+
+  return (
+  <>
+
+    <div style={{ marginBottom:10}}>{count}</div>
+    <div style={{display:'flex', flexDirection:'row', gap:10}}>
+      <button 
+        className="increase-button" 
+        onClick={(e) => {
+          setCount((previousCount) => previousCount+1);
+        }}
+      >
+        증가
+      </button>
+      <button
+        className="decrease-button"
+        onClick={(e) => {
+          setCount((previousCount) => previousCount-1);
+        }}
+      >
+        감소
+        </button>
+    </div>
+  </>);
 }
 
 export default App;
