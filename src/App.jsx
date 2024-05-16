af2ee4c482c4a47fa9ce780364443574a6124176
DOEKYONG <apochmn@naver.com>
2024-05-16 14:07:12 +0900

    5/state lifting async

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
 import "./App.css";
 
+const NON_ADULT_FEE = 10000
+const ADULT_FEE = 15000
 function App() {
   const [age, setAge] = useState(0);
   const [valid, setValid] = useState(false);
+  const [entrance,setEntrance] = useState(NON_ADULT_FEE);
 
 
   return (
   <>
+  <div>{entrance}</div>
     <input
       type="number"
       value={age}
-      onChange={ (e) => {
+      onChange={(e) => {
         const changed = e.target.value
         setAge(changed) 
-        setValid(changed >= 19)
+        // setValid((prev) => {
+        //   return prev >= 19
+        // })
+        const changedValid = changed >= 19;
+        setValid(changedValid)
+        setEntrance(changedValid ? ADULT_FEE : NON_ADULT_FEE)
       }}
     />
     {valid ? <div>성년입니다.</div> : <div style={{ color: 'red' }}>미성년입니다.</div>}
