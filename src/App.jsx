38d3b48d1ceaf56f40e4a19a0b2a7f55e8ddb1f9
DOEKYONG <apochmn@naver.com>
2024-05-16 14:59:28 +0900

    6-1/부모 자식 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,43 @@
 import { useState } from "react";
 import "./App.css";
 
+function ListItemComponent({children}) {
+  return <li>{children}</li>
+}
+
+function UnorderedListComponent() {
+  const arr = ['unordered 아이템 1','unordered 아이템 2','unordered 아이템 3']
+  // const reactArr = arr.map((each) => {
+  //   return <ListItemComponent>{each}</ListItemComponent>
+  // });
+  return (
+    // <ul>
+    //   <li>unordered 아이템 1</li>
+    //   <li>unordered 아이템 2</li>
+    //   <li>unordered 아이템 3</li>
+    // </ul>
+    <ul>
+      {
+        arr.map((each) => {
+          return <ListItemComponent>{each}</ListItemComponent>
+        })
+      }
+    </ul>
+
+  );
+}
+
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
   <>
-
-    <div style={{ marginBottom:10}}>{count}</div>
-    <div style={{display:'flex', flexDirection:'row', gap:10}}>
-      <button 
-        className="increase-button" 
-        onClick={(e) => {
-          setCount((previousCount) => previousCount+1);
-        }}
-      >
-        증가
-      </button>
-      <button
-        className="decrease-button"
-        onClick={(e) => {
-          setCount((previousCount) => previousCount-1);
-        }}
-      >
-        감소
-        </button>
+    <div>
+      <ol>
+      <ListItemComponent> ordered 아이템 1</ListItemComponent>
+      <ListItemComponent> ordered 아이템 2</ListItemComponent>
+      <ListItemComponent> ordered 아이템 3</ListItemComponent>
+      </ol>
+      <UnorderedListComponent/>
     </div>
   </>);
 }
