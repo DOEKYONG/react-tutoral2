a17320528abbefac600e0c9823f729071cbeecf9
DOEKYONG <apochmn@naver.com>
2024-05-16 16:24:58 +0900

    6-2 State Lifting 및 Props Drilling 그리고 값 바꿀수있게 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import { useState } from "react";
 import "./App.css";
 
-function ListItemComponent({children}) {
-  return <li>{children}</li>
+function ListItemComponent({item : person, children, setPeople}) {
+  const [activated,setActivated] = useState(false);
+  return (
+    <li onClick={ () => {setActivated(!activated)}}>
+      <div>{children}</div>
+      <div>{person.age}</div>
+      { activated ? <input type='text' value={person.desc}
+        onChange={(e) => {
+          const typed = e.target.value;
+          setPeople((prev) => {
+            const next = prev.map((each) => {
+              return person.name === each.name?{...each, desc:typed}:each
+            })
+            return next;
+          })
+        }}
+      /> : <div>{person.desc}</div>}
+    </li>)
 }
 
-function UnorderedListComponent() {
-  const arr = ['unordered 아이템 1','unordered 아이템 2','unordered 아이템 3']
-  // const reactArr = arr.map((each) => {
-  //   return <ListItemComponent>{each}</ListItemComponent>
-  // });
+function UnorderedListComponent({items,setPeople}) {
   return (
-    // <ul>
-    //   <li>unordered 아이템 1</li>
-    //   <li>unordered 아이템 2</li>
-    //   <li>unordered 아이템 3</li>
-    // </ul>
     <ul>
       {
-        arr.map((each) => {
-          return <ListItemComponent>{each}</ListItemComponent>
+        items.map((each) => {
+          return <ListItemComponent item={each} setPeople={setPeople}>{each.name}</ListItemComponent>
         })
       }
     </ul>
@@ -28,16 +35,19 @@ function UnorderedListComponent() {
 }
 
 function App() {
+  
+  const [people,setPeople] = useState( [
+    {name :'Aaron',age:10,desc:'안녕'},
+    {name :'Baron',age:30,desc:'하이'},
+    {name :'Caron',age:22,desc:'바보'},
+    {name :'Daron',age:17,desc:'바이'}
+    ])
 
   return (
   <>
     <div>
-      <ol>
-      <ListItemComponent> ordered 아이템 1</ListItemComponent>
-      <ListItemComponent> ordered 아이템 2</ListItemComponent>
-      <ListItemComponent> ordered 아이템 3</ListItemComponent>
-      </ol>
-      <UnorderedListComponent/>
+      {/* <ListItemComponent/> */}
+      <UnorderedListComponent items={people} setPeople={setPeople}/>
     </div>
   </>);
 }
