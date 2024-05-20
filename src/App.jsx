263104aba3de45410f0df88dfe56168083adce1f
DOEKYONG <apochmn@naver.com>
2024-05-20 11:08:27 +0900

    ref input은 랜더링 되지 않음

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@ import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  // const ageReferecne = useRef(0);
-  const [age,setAge] = useState(0);
+  const ageReferecne = useRef(0);
+  // const [age,setAge] = useState(0);
 
   // 리랜더 일어나는지 콘솔로그
   console.log("리랜더");
   return (
   <>
+  {/* ref.current 를 view 에 사용한느 멍청한짓은 하지말자 */}
   <div>
-    {age}
+    {ageReferecne.current}
   </div>
-  <input type="number" value={age} onChange={(e) => setAge(e.target.value)} />
-  </>);
+  <input type="number"
+    ref={ageReferecne} 
+  // value={age} onChange={(e) => {ageReferecne.current = e.target.value}} 
+  />
+  <button onClick={ () => { console.log('지금 값은 : ', ageReferecne.current.value)}}>표기</button>
+  </>
+  );
 }
 
 export default App;
