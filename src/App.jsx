f48f6b1c3d480764b427ab9458b95b17a91814bd
DOEKYONG <apochmn@naver.com>
2024-05-20 11:24:16 +0900

    ref를 활용한 input 폼 validation

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,18 @@ import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [valid,setValid] = useState(false);
   const ageReferecne = useRef(0);
-  // const [age,setAge] = useState(0);
 
   // 리랜더 일어나는지 콘솔로그
   console.log("리랜더");
   return (
   <>
   {/* ref.current 를 view 에 사용한느 멍청한짓은 하지말자 */}
-  <div>
-    {ageReferecne.current}
-  </div>
-  <input type="number"
-    ref={ageReferecne} 
-  // value={age} onChange={(e) => {ageReferecne.current = e.target.value}} 
-  />
-  <button onClick={ () => { console.log('지금 값은 : ', ageReferecne.current.value)}}>표기</button>
+  <input type="number" onChange={(e) => {
+    e.target.value>=19?setValid(true):setValid(false);
+  }}/>
+  {valid ?<div>성년입니다.</div> : <div style={{color: 'red'}}>미성년입니다.</div>}
   </>
   );
 }
