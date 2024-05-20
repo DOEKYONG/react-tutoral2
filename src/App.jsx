58f83beaf22bf4ecd610536e20c2de5b5aee607a
DOEKYONG <apochmn@naver.com>
2024-05-20 10:52:50 +0900

    useRef no-Render

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
   const countReference = useRef(0);
+  // Render 가 일어날 때마다 표기 | countReference 를 '조작'하고, '표기 했을 때 어떠한 Render도 발생하지 않음
   return (
   <>
     <div>{countReference.current}</div>
