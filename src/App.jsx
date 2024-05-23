96a1963e761fc4b62374e05d3677ce714dbfa9bc
DOEKYONG <apochmn@naver.com>
2024-05-23 14:30:44 +0900

    context

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,73 @@
-import { useState } from "react";
+import { createContext, useContext, useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
 
-  return (
+function Header() {
+  const {theme,setTheme} = useContext(ThemeContext)
+  return(
   <>
+    <button onClick={()=>{
+      if(theme==='blue') {
+        setTheme('red')
+      } else {
+        setTheme('blue')
+      }
+    }}>테마변경</button>
+  </>)
+}
 
-    <div style={{ marginBottom:10}}>{count}</div>
-    <div style={{display:'flex', flexDirection:'row', gap:10}}>
-      <button 
-        className="increase-button" 
-        onClick={(e) => {
-          setCount((previousCount) => previousCount+1);
+function ButtonComponent() {
+return(
+     <ThemeContext.Consumer>
+        {({theme}) => {
+          return <button style={{color:theme}} >로그인하기</button>
         }}
-      >
-        증가
-      </button>
-      <button
-        className="decrease-button"
-        onClick={(e) => {
-          setCount((previousCount) => previousCount-1);
-        }}
-      >
-        감소
-        </button>
-    </div>
-  </>);
+      </ThemeContext.Consumer> 
+)
+
+}
+
+function PasswordInputComponent() {
+  // 3. 컨슈머타입 유즈컨텍스트 타입
+  const {theme} = useContext(ThemeContext);
+  return <input type="password" style={{color:theme}} />
+}
+
+function IdInputComponent() {
+  // 3. 컨슈머타입 유즈컨텍스트 타입
+  const {theme} = useContext(ThemeContext);
+  return <input type="text" style={{color:theme}} />
+}
+
+
+function LoginComponent() {
+  return (
+    <>
+      <IdInputComponent  />
+      <PasswordInputComponent  />
+      <ButtonComponent  />
+    </>
+  )
+
+}
+const ThemeContext = createContext('red');
+
+
+function ThemeContextProvider({children}) {
+  const [theme,setTheme] = useState('red')
+  return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>
+}
+// 3. Consumer
+function App() {
+  return (
+    <>
+      <ThemeContextProvider>
+        <Header/>
+      <LoginComponent />
+      </ThemeContextProvider>
+    </>
+  )
+    
 }
 
 export default App;
