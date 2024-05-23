d728df4dc55bedd4c775befc13708fe841ab6cff
DOEKYONG <apochmn@naver.com>
2024-05-23 15:41:39 +0900

    customHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
 import { useState } from "react";
 import "./App.css";
 
+function usePersonSelect(initialvalue) {
+  const [person, setPerson] = useState(initialvalue)
+ 
+  return [person,
+          () =>{setPerson((perv)=>perv +1 )},
+          () =>{setPerson((prev)=> prev-1 )}
+        ];
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [adults, plusAdults, minusAdults] = usePersonSelect(0);
+  const [kids,plusKids,minusKids] = usePersonSelect(0);
+  const [babies,plusBabies,minusBabies] =usePersonSelect(0);
+  const [pets,pulsPets,minusPets] = usePersonSelect(0);
 
   return (
   <>
-
-    <div style={{ marginBottom:10}}>{count}</div>
-    <div style={{display:'flex', flexDirection:'row', gap:10}}>
-      <button 
-        className="increase-button" 
-        onClick={(e) => {
-          setCount((previousCount) => previousCount+1);
-        }}
-      >
-        증가
-      </button>
-      <button
-        className="decrease-button"
-        onClick={(e) => {
-          setCount((previousCount) => previousCount-1);
-        }}
-      >
-        감소
-        </button>
+    <div>성인
+      <button onClick={ plusAdults}>+</button>{adults}<button onClick={ minusAdults}>-</button>
     </div>
+    <div>어린이
+    <button onClick={ plusKids}>+</button>{kids}<button onClick={ minusKids} >-</button>
+      </div>    
+    <div>유아
+    <button onClick={ plusBabies}>+</button>{babies}<button onClick={minusBabies}>-</button>
+    </div>
+    <div>반려동물
+    <button onClick={ pulsPets}>+</button>{pets}<button onClick={ minusPets} >-</button>
+    </div>
+
+
   </>);
 }
 
